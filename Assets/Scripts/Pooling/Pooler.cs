fa0402dd772ade351a1d5547d52b93ded77c3a34
teachers <teachers>
2023-04-19 18:25:15 +0200

    pooling

---
+++ Assets/Scripts/Pooling/Pooler.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pooler : MonoBehaviour
+{
+
+    public PoolObject poolableObject;
+    public int spawnNumber;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for(int i = 0; i< spawnNumber; i++)
+        {
+            SpawnNewObjectPool();
+        }
+    }
+
+    public void GetObject(Transform player)
+    {
+        for(int i = 0; i< transform.childCount; i++)
+        {
+            if (!transform.GetChild(i).gameObject.activeInHierarchy)
+            {
+                transform.GetChild(i).SetParent(player);
+                return;
+            }
+        }
+        var go = SpawnNewObjectPool();
+        go.transform.SetParent(player);
+        go.gameObject.SetActive(true);
+    }
+
+    public PoolObject SpawnNewObjectPool()
+    {
+        var pool = Instantiate(poolableObject, transform);
+        pool.pooler = this;
+        pool.gameObject.SetActive(false);
+        return pool;
+    }
+}
